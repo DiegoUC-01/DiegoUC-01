992200b0105f02d10d3d1d0b90a930e632f7a5e2
DiegoUC-01 <88514354+DiegoUC-01@users.noreply.github.com>
2026-03-25 18:56:16 -0300

    Fix indentation in AES cipher initialization

--- aes-cli/encriptador.py
+++ aes-cli/encriptador.py
@@ -11,8 +11,7 @@ def encrypt_text(key: bytes, plaintext: str) -> bytes:
     iv = os.urandom(16)
 
     # Crear cifrador AES en modo CBC
-    cipher = Cipher
-        (algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
 
     # Aplicar padding PKCS7
